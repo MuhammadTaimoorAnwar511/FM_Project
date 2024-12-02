60faeac4bd5ad56ff30dae72a032ca38596e29e6
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2024-12-02 23:21:51 +0500

    3rd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from feature_model_parser import validate_xml, parse_feature_model
+from translator import translate_to_propositional_logic
 from solver import compute_mwps
 from visualization import create_checkbox_tree
 
@@ -12,18 +13,23 @@ def main():
     feature_model = parse_feature_model(xml_tree)
     print("Feature model parsed successfully.\n")
 
-    # Step 2: Compute MWPs
+    # Step 2: Translate to Propositional Logic
+    print("Propositional Logic Formula:\n")
+    logic_expressions = translate_to_propositional_logic(feature_model)
+
+
+    # Step 3: Compute MWPs
     print("Computing Minimum Working Products (MWPs)...")
     mwps = compute_mwps(feature_model)
 
-    # Step 3: Visualize Feature Model (Optional)
+    # Step 4: Visualize Feature Model (Optional)
     print("\nVisualizing the feature model...")
     create_checkbox_tree(feature_model)
     
     # Final Output
-    print("\n===== Final MWPs =====")
-    for idx, mwp in enumerate(mwps, start=1):
-        print(f"MWP {idx}: {', '.join(mwp)}")
+    #print("\n===== Final MWPs =====")
+    #for idx, mwp in enumerate(mwps, start=1):
+    #    print(f"MWP {idx}: {', '.join(mwp)}")
     
     print("\nProgram Execution Complete!")
 
